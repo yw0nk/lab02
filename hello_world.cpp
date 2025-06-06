70fb5cc4c045a24192d0358d1d202a4200830932
yw0nk <bogtkachenko@gmail.com>
2025-06-06 20:42:29 +0000

    Formatted with clang-format (Mozilla style)

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
-int main() {
-    string name;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Hello, world from " << name << "!";
-    return 0;
+int
+main()
+{
+  string name;
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Hello, world from " << name << "!";
+  return 0;
 }
